4b858732381968ca865251bf969da4448528a92d
Naris <nrazafimahatratra@softwaymedical.fr>
2023-02-13 10:46:56 +0300

    💄 Add style to active link

--- components/drawer/menu-item.vue
+++ components/drawer/menu-item.vue
@@ -1,25 +1,27 @@
 <template>
-  <NuxtLink :to="path">
-    <li class="flex space-x-4 p-4 rounded-md hover:bg-primary-container hover:text-on-primary-container" :class="{ 'bg-primary text-on-primary': selected }">
+  <li>
+    <NuxtLink :to="path" class="flex space-x-4 p-4 rounded-md hover:bg-primary-container hover:text-on-primary-container">
       <div class="w-6">
         <slot name="icon"></slot>
       </div>
       <div>
         <slot></slot>
       </div>
-    </li>
-  </NuxtLink>
+    </NuxtLink>
+  </li>
 </template>
 
 <script setup>
 defineProps({
-  selected: {
-    type: Boolean,
-    default: false
-  },
   path: {
     type: String,
     required: true
   }
 })
 </script>
+
+<style>
+.router-link-active {
+  @apply bg-primary text-on-primary
+}
+</style>
