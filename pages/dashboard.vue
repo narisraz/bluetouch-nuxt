72cf5b1c4f02e68ecf98566229637d0a782c01b0
Naris <nrazafimahatratra@softwaymedical.fr>
2023-02-13 13:28:19 +0300

    💄 Add some fake data to dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,9 +1,35 @@
 <template>
-  <div class="grid grid-cols-4 gap-4">
-    <div class="flex bg-surface rounded-md space-x-4 items-center">
-      <div class="p-4 font-bold bg-primary-container text-on-primary-container rounded-l-md">4</div>
-      <div>Clients</div>
+  <div class="space-y-4">
+    <div class="grid grid-cols-3 gap-4">
+      <div class="flex bg-surface rounded-md space-x-4 items-center">
+        <div class="p-4 font-bold bg-primary-container text-on-primary-container rounded-l-md">4</div>
+        <div class="uppercase">Bornes fontaines</div>
+      </div>
+      <div class="flex bg-surface rounded-md space-x-4 items-center">
+        <div class="p-4 font-bold bg-primary-container text-on-primary-container rounded-l-md">4</div>
+        <div class="uppercase">Branchements privés</div>
+      </div>
+      <div class="flex bg-surface rounded-md space-x-4 items-center">
+        <div class="p-4 font-bold bg-primary-container text-on-primary-container rounded-l-md">4</div>
+        <div class="uppercase">Branchements sociaux</div>
+      </div>
     </div>
+
+    <div class="grid grid-cols-3 gap-4">
+      <div class="flex bg-surface rounded-md space-x-4 items-center">
+        <div class="p-4 font-bold bg-primary-container text-on-primary-container rounded-l-md">4</div>
+        <div class="uppercase">Factures émises</div>
+      </div>
+      <div class="flex bg-surface rounded-md space-x-4 items-center">
+        <div class="p-4 font-bold bg-primary-container text-on-primary-container rounded-l-md">4</div>
+        <div class="uppercase">Volume relevé</div>
+      </div>
+      <div class="flex bg-surface rounded-md space-x-4 items-center">
+        <div class="p-4 font-bold bg-primary-container text-on-primary-container rounded-l-md">4</div>
+        <div class="uppercase">Volume facturé</div>
+      </div>
+    </div>
+    
   </div>
 </template>
 
