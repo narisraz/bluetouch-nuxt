7b60579e69754d6038ada16b49089f9f036778ec
Naris <nrazafimahatratra@softwaymedical.fr>
2023-02-13 11:26:12 +0300

    💄 Change table border style

--- pages/home.vue
+++ pages/home.vue
@@ -2,40 +2,40 @@
   <table class="table-auto border-spacing-3 w-full bg-surface text-on-surface">
     <tbody>
       <tr class="p-8">
-        <td class="border-b-[1px] py-2 px-3 font-semibold">Nom de la commune</td>
-        <td class="border-b-[1px]">Sabotsy Namehana</td>
+        <td class="border-b-2 border-background py-2 px-3 font-semibold">Nom de la commune</td>
+        <td class="border-b-2 border-background">Sabotsy Namehana</td>
       </tr>
       <tr>
-        <td class="border-b-[1px] py-2 px-3 font-semibold">Nombre de population</td>
-        <td class="border-b-[1px]">20 000</td>
+        <td class="border-b-2 border-background py-2 px-3 font-semibold">Nombre de population</td>
+        <td class="border-b-2 border-background">20 000</td>
       </tr>
       <tr>
-        <td class="border-b-[1px] py-2 px-3 font-semibold">District</td>
-        <td class="border-b-[1px]">Avaradrano</td>
+        <td class="border-b-2 border-background py-2 px-3 font-semibold">District</td>
+        <td class="border-b-2 border-background">Avaradrano</td>
       </tr>
       <tr>
-        <td class="border-b-[1px] py-2 px-3 font-semibold">Région</td>
-        <td class="border-b-[1px]">Analamanga</td>
+        <td class="border-b-2 border-background py-2 px-3 font-semibold">Région</td>
+        <td class="border-b-2 border-background">Analamanga</td>
       </tr>
       <tr>
-        <td class="border-b-[1px] py-2 px-3 font-semibold">Localisation sur la carte</td>
-        <td class="border-b-[1px]">
+        <td class="border-b-2 border-background py-2 px-3 font-semibold">Localisation sur la carte</td>
+        <td class="border-b-2 border-background">
           <NuxtLink to="/" class="text-primary">
             <IconArrowTopRightOnSquare />
           </NuxtLink>
         </td>
       </tr>
       <tr>
-        <td class="border-b-[1px] py-2 px-3 font-semibold">Ressource en eau</td>
-        <td class="border-b-[1px]">source de montagne, réservoir, rivière</td>
+        <td class="border-b-2 border-background py-2 px-3 font-semibold">Ressource en eau</td>
+        <td class="border-b-2 border-background">source de montagne, réservoir, rivière</td>
       </tr>
       <tr>
-        <td class="border-b-[1px] py-2 px-3 font-semibold">Nombre de réservoir</td>
-        <td class="border-b-[1px]">2</td>
+        <td class="border-b-2 border-background py-2 px-3 font-semibold">Nombre de réservoir</td>
+        <td class="border-b-2 border-background">2</td>
       </tr>
       <tr>
-        <td class="border-b-[1px] py-2 px-3 font-semibold">Capacité total réservoir (m3)</td>
-        <td class="border-b-[1px]">150</td>
+        <td class="border-b-2 border-background py-2 px-3 font-semibold">Capacité total réservoir (m3)</td>
+        <td class="border-b-2 border-background">150</td>
       </tr>
     </tbody>
   </table>
