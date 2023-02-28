0d371ef7bccf44a6778c9f490bc5285d3b108041
Naris Razafimahatratra <naris.razafimahatratra@gmail.com>
2023-02-28 11:22:53 +0300

    ✨ Create generer tournée page

--- pages/clients/generer-tournee.vue
+++ pages/clients/generer-tournee.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="space-y-4">
-    <div class="flex items-center justify-between">
-      <div class="text-on-background uppercase font-bold">Liste des clients ({{ displayCount(clients.meta.pagination) }})</div>
-      <NuxtLink to="/clients/new">
-        <Button class="bg-secondary text-on-secondary flex space-x-2 hover:bg-secondary/75">
-          <IconPlus />
-          <div>
-            Nouveau
-          </div>
-        </Button>
-      </NuxtLink>
-    </div>
+    <ContainerCard title="Encaissement">
+      <form class="flex space-x-4 items-end">
+        <FormSelect name="tournee" label="Tournée" :options="tournees" />
+        <FormSelect name="responsable" label="Responsable" :options="responsables" />
+        <div>
+          <Button class="bg-secondary text-on-secondary">Générer</Button>
+        </div>
+      </form>
+    </ContainerCard>
 
     <div class="w-full">
       <Table>
@@ -18,12 +16,11 @@
           <tr>
             <TableTh>N° Contrat</TableTh>
             <TableTh>Abonné</TableTh>
-            <TableTh>Groupe tarifaire</TableTh>
             <TableTh>Compteur</TableTh>
             <TableTh>Tél. mobile</TableTh>
             <TableTh>Rue</TableTh>
             <TableTh>Adresse</TableTh>
-            <TableTh>Taille de ménage</TableTh>
+            <TableTh>Rang dans la tournée</TableTh>
             <TableTh is-last>Etat du branchement</TableTh>
           </tr>
         </TableThead>
@@ -31,12 +28,11 @@
           <tr v-for="client in clients.data" :key="client.id">
             <TableTd>{{ client.attributes.num_contrat }}</TableTd>
             <TableTd>{{ client.attributes.nom }} {{ client.attributes.prenom }}</TableTd>
-            <TableTd>{{ displayBranchement(client.attributes.branchement) }}</TableTd>
             <TableTd>{{ displayCompteur(client.attributes.compteur) }}</TableTd>
             <TableTd>{{ client.attributes.tel }}</TableTd>
             <TableTd>{{ displayRue(client.attributes.adresse) }}</TableTd>
             <TableTd>{{ displayAdresse(client.attributes.adresse) }}</TableTd>
-            <TableTd>{{ client.attributes.taille_menage }}</TableTd>
+            <TableTd></TableTd>
             <TableTd is-last>{{ displayEtatBranchement(client.attributes.etat_branchement) }}</TableTd>
           </tr>
         </tbody>
@@ -61,6 +57,9 @@ useHead({
 const { find } = useStrapi()
 const saepStore = useSaepStore()
 
+const tournees: Option[] = []
+const responsables: Option[] = []
+
 const clients = await find<Client>('clients', {
   filters: {
     saep: saepStore.saep.id
@@ -72,10 +71,6 @@ const clients = await find<Client>('clients', {
     etat_branchement: true
   }
 })
-
-function displayBranchement(branchement: Strapi4ResponseSingle<Branchement> | number) {
-  return (branchement as Strapi4ResponseSingle<Branchement>).data.attributes.label
-}
 
 function displayCompteur(compteur: Strapi4ResponseSingle<Compteur> | number) {
   return (compteur as Strapi4ResponseSingle<Compteur>).data.attributes.identifiant
@@ -91,9 +86,5 @@ function displayAdresse(adresse: Strapi4ResponseSingle<Adresse> | number) {
 
 function displayEtatBranchement(etatBranchement: Strapi4ResponseSingle<EtatBranchement> | number) {
   return (etatBranchement as Strapi4ResponseSingle<EtatBranchement>).data.attributes.label
-}
-
-function displayCount(pagination: any) {
-  return pagination.total
 }
 </script>
